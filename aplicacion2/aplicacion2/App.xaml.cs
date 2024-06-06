deafd2ad37df909483af792de1e1d103854d77cb
Jonathan Toca <jonathan.fulmh@gmail.com>
2024-06-05 23:13:55 -0500

    Se agrega el proyeto

---
+++ aplicacion2/aplicacion2/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace aplicacion2
+{
+    public partial class App : Application
+    {
+        public static MasterDetailPage MAsterDet {  get; set; }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ aplicacion2/aplicacion2/Master.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace aplicacion2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : ContentPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnNosotros_Clicked(object sender, EventArgs e)
+        {
+            App.MAsterDet.IsPresented = false;
+            await App.MAsterDet.Detail.Navigation.PushAsync(new Nosotros());
+
+        }
+
+        private async void btnProductos_Clicked(object sender, EventArgs e)
+        {
+            App.MAsterDet.IsPresented = false;
+            await App.MAsterDet.Detail.Navigation.PushAsync(new Productos());
+
+
+        }
+
+        private async void btnUbicacion_Clicked(object sender, EventArgs e)
+        {
+            App.MAsterDet.IsPresented = false;
+            await App.MAsterDet.Detail.Navigation.PushAsync(new Ubicacion());
+
+        }
+
+        private async void btnContactos_Clicked(object sender, EventArgs e)
+        {
+            App.MAsterDet.IsPresented = false;
+            await App.MAsterDet.Detail.Navigation.PushAsync(new Contactos());
+
+        }
+    }
+}
